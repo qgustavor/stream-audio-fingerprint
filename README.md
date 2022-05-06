be7ed4c4edd1c1a8b0f0afc48c7ed921b9d455c4
qgustavor <qgustavor@users.noreply.github.com>
2022-05-06 00:24:03 -0300

    First fork changes
    Migrate to Standard
    Migrate to esbuild
    Fix linting issues
    Remove yarn.lock
    Move package to user scope
    Keep example code only in readme to avoid duplication
    Fix example
    Remove readme section which was never updated
    Fix cat example to work in Windows without needing a second example
    Update the example to use the future intended API

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# Audio landmark fingerprinting as a Node Stream module
+# Audio landmark fingerprinting as a JavaScript module
 
-This module is a duplex stream (instance of stream.Transform) that converts a PCM audio signal into a series of audio fingerprints. It works with audio tracks as well as with unlimited audio streams, e.g. broadcast radio.
+This module is [a transform stream](https://developer.mozilla.org/en-US/docs/Web/API/TransformStream) that converts a PCM audio signal into a series of audio fingerprints. It works with audio tracks as well as with unlimited audio streams, e.g. broadcast radio.
 
-It is one of the foundations of the [Adblock Radio project](https://github.com/adblockradio/adblockradio).
+It's based on [lpolito/stream-audio-fingerprint](https://github.com/lpolito/stream-audio-fingerprint) which is based [adblockradio/stream-audio-fingerprint](https://github.com/adblockradio/stream-audio-fingerprint) which is one of the foundations of the [Adblock Radio project](https://github.com/adblockradio/adblockradio).
 
 ## Credits
 
@@ -29,52 +29,44 @@ Given the same audio, this figure shows the same peaks and the internal *forward
 ## Usage
 
 ```sh
-npm install stream-audio-fingerprint
+npm install @qgustavor/stream-audio-fingerprint
 ```
 
 The algorithm is in ```codegen_landmark.js```.
 
-A demo usage is proposed in ```codegen_demo.js```. It requires the executable [ffmpeg](https://ffmpeg.org/download.html) to run.
+A demo usage is proposed below. It requires the executable [ffmpeg](https://ffmpeg.org/download.html) to run.
 
 ```javascript
-var decoder = require('child_process').spawn('ffmpeg', [
+import { spawn } 'child_process'
+import Codegen from 'stream-audio-fingerprint'
+
+const decoder = spawn('ffmpeg', [
 	'-i', 'pipe:0',
 	'-acodec', 'pcm_s16le',
 	'-ar', 22050,
 	'-ac', 1,
-	'-f', 'wav',
+	'-f', 's16le',
 	'-v', 'fatal',
 	'pipe:1'
-], { stdio: ['pipe', 'pipe', process.stderr] });
-process.stdin.pipe(decoder.stdin);
+], { stdio: ['pipe', 'pipe', process.stderr] })
+process.stdin.pipe(decoder.stdin)
 
-var Codegen = require("stream-audio-fingerprint");
-var fingerprinter = new Codegen();
-decoder.stdout.pipe(fingerprinter);
+const fingerprinter = new Codegen()
+decoder.stdout.pipe(fingerprinter.writable)
 
-fingerprinter.on("data", function(data) {
-	for (var i=0; i<data.tcodes.length; i++) {
-		console.log("time=" + data.tcodes[i] + " fingerprint=" + data.hcodes[i]);
+for await (const data of fingerprinter.readable) {
+	for (let i=0; i < data.tcodes.length; i++) {
+		console.log(`time=${data.tcodes[i]} fingerprint=${data.hcodes[i]}`)
 	}
-});
+})
 ```
 
 and then we pipe audio data, either a stream or a file
 
 ```sh
-curl http://radiofg.impek.com/fg | nodejs codegen_demo.js
-cat awesome_music.mp3 | nodejs codegen_demo.js
+curl http://radiofg.impek.com/fg | node codegen_demo.mjs
+node codegen_demo.mjs < awesome_music.mp3
 ```
-on Windows:
-```
-type awesome_music.mp3 | node codegen_demo.js
-```
-
-## Integration in your project
-
-Matching fingerprints in a database is not a trivial topic, I should write a technical note about it some day.
-
-For a reference implementation you can have a look at the code of the Adblock Radio algorithm to catch ads https://github.com/adblockradio/adblockradio/blob/master/predictor-db/hotlist.js#L150.
 
 ## License
 
--- src/lib/fft.ts
+++ src/lib/fft.ts
@@ -1,4 +1,3 @@
-/* eslint-disable no-mixed-operators, max-len, no-bitwise, no-plusplus, @typescript-eslint/no-use-before-define, no-param-reassign, no-throw-literal */
 // Original from: https://www.nayuki.io/page/free-small-fft-in-multiple-languages
 // Modified by Chris Cannam: https://code.soundsoftware.ac.uk/projects/js-dsp-test/repository/entry/fft/nayuki-obj/fft.js
 
@@ -30,114 +29,107 @@
  * Typescript from: https://www.nayuki.io/res/free-small-fft-in-multiple-languages/fft.ts
  */
 
-
 /*
  * Construct an object for calculating the discrete Fourier transform (DFT) of size n, where n is a power of 2.
  */
 class FFTNayuki {
-    private n: number;
+  private readonly n: number
+  private readonly levels: number
+  private readonly cosTable: number[]
+  private readonly sinTable: number[]
+  public spectrum: number[]
+  public peakBand = 0
+  public peak = 0
 
-    private levels: number;
+  /**
+    * @param n Buffer size.
+    */
+  constructor (n: number) {
+    this.n = n
 
-    private cosTable: number[];
-
-    private sinTable: number[];
-
-    public spectrum: number[];
-
-    public peakBand = 0;
-
-    public peak = 0;
-
-    /**
-     * @param n Buffer size.
-     */
-    constructor(n: number) {
-      this.n = n;
-
-      this.levels = -1;
-      for (let i = 0; i < 32; i++) {
-        if (1 << i === n) { this.levels = i; } // Equal to log2(n)
-      }
-      if (this.levels === -1) {
-        throw 'Length is not a power of 2';
-      }
-
-      // Trigonometric tables
-      this.cosTable = new Array(n / 2);
-      this.sinTable = new Array(n / 2);
-      for (let i = 0; i < n / 2; i++) {
-        this.cosTable[i] = Math.cos(2 * Math.PI * i / n);
-        this.sinTable[i] = Math.sin(2 * Math.PI * i / n);
-      }
-
-      this.spectrum = new Array(n / 4);
+    this.levels = -1
+    for (let i = 0; i < 32; i++) {
+      if (1 << i === n) { this.levels = i } // Equal to log2(n)
+    }
+    if (this.levels === -1) {
+      throw Error('Length is not a power of 2')
     }
 
-    /*
-     * Computes the discrete Fourier transform (DFT) of the given complex vector, storing the result back into the vector.
-     * The vector's length must be equal to the size n that was passed to the object constructor, and this must be a power of 2. Uses the Cooley-Tukey decimation-in-time radix-2 algorithm.
-     */
-    public forward(real: Array<number>|Float64Array, imag: Array<number>|Float64Array): void {
-      // Bit-reversed addressing permutation
-      for (let i = 0; i < this.n; i++) {
-        const j: number = reverseBits(i, this.levels);
-        if (j > i) {
-          let temp: number = real[i];
-          real[i] = real[j];
-          real[j] = temp;
-          temp = imag[i];
-          imag[i] = imag[j];
-          imag[j] = temp;
-        }
-      }
+    // Trigonometric tables
+    this.cosTable = new Array(n / 2)
+    this.sinTable = new Array(n / 2)
+    for (let i = 0; i < n / 2; i++) {
+      this.cosTable[i] = Math.cos(2 * Math.PI * i / n)
+      this.sinTable[i] = Math.sin(2 * Math.PI * i / n)
+    }
 
-      // Cooley-Tukey decimation-in-time radix-2 FFT
-      for (let size = 2; size <= this.n; size *= 2) {
-        const halfsize: number = size / 2;
-        const tablestep: number = this.n / size;
-        for (let i = 0; i < this.n; i += size) {
-          for (let j = i, k = 0; j < i + halfsize; j++, k += tablestep) {
-            const l: number = j + halfsize;
-            const tpre: number = real[l] * this.cosTable[k] + imag[l] * this.sinTable[k];
-            const tpim: number = -real[l] * this.sinTable[k] + imag[l] * this.cosTable[k];
-            real[l] = real[j] - tpre;
-            imag[l] = imag[j] - tpim;
-            real[j] += tpre;
-            imag[j] += tpim;
-          }
-        }
-      }
+    this.spectrum = new Array(n / 4)
+  }
 
-      this.calculateSpectrum(real, imag);
-
-      // Returns the integer whose value is the reverse of the lowest 'bits' bits of the integer 'x'.
-      function reverseBits(x: number, bits: number): number {
-        let y = 0;
-        for (let i = 0; i < bits; i++) {
-          y = (y << 1) | (x & 1);
-          x >>>= 1;
-        }
-        return y;
+  /*
+   * Computes the discrete Fourier transform (DFT) of the given complex vector, storing the result back into the vector.
+   * The vector's length must be equal to the size n that was passed to the object constructor, and this must be a power of 2. Uses the Cooley-Tukey decimation-in-time radix-2 algorithm.
+   */
+  public forward (real: number[]|Float64Array, imag: number[]|Float64Array): void {
+    // Bit-reversed addressing permutation
+    for (let i = 0; i < this.n; i++) {
+      const j: number = reverseBits(i, this.levels)
+      if (j > i) {
+        let temp: number = real[i]
+        real[i] = real[j]
+        real[j] = temp
+        temp = imag[i]
+        imag[i] = imag[j]
+        imag[j] = temp
       }
     }
 
-    private calculateSpectrum(real: Array<number>|Float64Array, imag: Array<number>|Float64Array): void {
-      // Should this be 4 / buffersize?
-      const bSi = 4 / this.n;
-      let mag;
-
-      for (let i = 0, N = this.n / 4; i < N; i++) {
-        mag = bSi * Math.sqrt(real[i] ** 2 + imag[i] ** 2);
-
-        if (mag > this.peak) {
-          this.peakBand = i;
-          this.peak = mag;
+    // Cooley-Tukey decimation-in-time radix-2 FFT
+    for (let size = 2; size <= this.n; size *= 2) {
+      const halfsize: number = size / 2
+      const tablestep: number = this.n / size
+      for (let i = 0; i < this.n; i += size) {
+        for (let j = i, k = 0; j < i + halfsize; j++, k += tablestep) {
+          const l: number = j + halfsize
+          const tpre: number = real[l] * this.cosTable[k] + imag[l] * this.sinTable[k]
+          const tpim: number = -real[l] * this.sinTable[k] + imag[l] * this.cosTable[k]
+          real[l] = real[j] - tpre
+          imag[l] = imag[j] - tpim
+          real[j] += tpre
+          imag[j] += tpim
         }
-
-        this.spectrum[i] = mag;
       }
     }
+
+    this.calculateSpectrum(real, imag)
+
+    // Returns the integer whose value is the reverse of the lowest 'bits' bits of the integer 'x'.
+    function reverseBits (x: number, bits: number): number {
+      let y = 0
+      for (let i = 0; i < bits; i++) {
+        y = (y << 1) | (x & 1)
+        x >>>= 1
+      }
+      return y
+    }
+  }
+
+  private calculateSpectrum (real: number[]|Float64Array, imag: number[]|Float64Array): void {
+    // Should this be 4 / buffersize?
+    const bSi = 4 / this.n
+    let mag
+
+    for (let i = 0, N = this.n / 4; i < N; i++) {
+      mag = bSi * Math.sqrt(real[i] ** 2 + imag[i] ** 2)
+
+      if (mag > this.peak) {
+        this.peakBand = i
+        this.peak = mag
+      }
+
+      this.spectrum[i] = mag
+    }
+  }
 }
 
-export default FFTNayuki;
+export default FFTNayuki
